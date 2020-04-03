44edf6e60823e523b180cc4985aad313180ef669
ASUS <ASUS@LAPTOP-2RINFD0B>
2020-04-03 15:05:30 +0800

    添加项目文件。

---
+++ NewHope/Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewHope.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 母版页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Mother()
+        {
+            return View();
+        }
+
+
+
+    }
+}
